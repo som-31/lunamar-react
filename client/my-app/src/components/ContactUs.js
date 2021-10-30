e94a8a280de6d015b1d8c952696bb64cd2e9b6ce
Somnath Jadhav <somnath3151995@gmail.com>
2021-10-30 16:13:45 -0500

    fixed routing and added files for about contact employee header login serviceInformation

--- client/my-app/src/components/ContactUs.js
+++ client/my-app/src/components/ContactUs.js
@@ -4,7 +4,21 @@ function ContactUs(){
 
     return(
         <>
-        <h1> This is contact us page</h1>
+            <div class="container">
+                    <div class="contact-box">
+                        <div class="left"></div>
+                        <div class="right">
+                            <center><h2>Contact Us</h2></center>
+                            <form action="" id="register" method="post">
+                                <input type="text" class="field" id="name" placeholder="Your Name" />
+                                <input type="text" class="field" id="email" placeholder="Your Email" />
+                                <input type="text" class="field" placeholder="Phone" />
+                                <textarea placeholder="Query" id="query" class="field"></textarea>
+                                <input type="button" value="Submit" onclick="sendEmail()" class="submit-btn" />
+                            </form>
+                        </div>
+                    </div>
+                </div>
         </>
     );
 
