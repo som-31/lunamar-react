4fd148312d097c3cc373bd66613a069fd19d0130
Somnath Jadhav <somnath3151995@gmail.com>
2021-10-30 14:17:42 -0500

    classname

--- client/my-app/src/components/AboutUs.js
+++ client/my-app/src/components/AboutUs.js
@@ -4,7 +4,7 @@ function AboutUs(){
     return (
         <>
         
-    {/* <div class="navbar">
+    {/* <div className="navbar">
         <div>
                     <img src="assets/images/Lunamar-logos.jpeg" alt="subdivision image" height="20%" width="30%" />
 
@@ -21,20 +21,20 @@ function AboutUs(){
         </nav>
     </div> */}
 
-	<div class="section">
-		<div class="container">
-			<div class="content-section">
-				<div class="title">
+	<div className="section">
+		<div className="container">
+			<div className="content-section">
+				<div className="title">
 					<h1>About Us</h1>
 				</div>
             
-				<div class="content">
+				<div className="content">
 					<h3>Lorem ipsum dolor sit amet, consectetur adipisicing</h3>
 					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
 					consequat.</p>
-					<div class="social">
+					<div className="social">
                         <a href="https://www.facebook.com/" target="_blank"><img src="assets/images/facebook.png" alt="facebook" width="30" height="30" /></a>
                         <a href="https://www.instagram.com/" target="_blank"><img src="assets/images/instagram.png" alt="instagram" width="30" height="30" /></a>
                         <a href="https://twitter.com/" target="_blank"><img src="assets/images/twitter.png" alt="twitter" width="30" height="30" /></a>
@@ -42,7 +42,7 @@ function AboutUs(){
 				</div>
 				
 			</div>
-			<div class="image-section">
+			<div className="image-section">
                 <img src="assets/images/Logo.png" alt="buildings image" />
 			</div>
 		</div>
