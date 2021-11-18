6df119cabf47cdb4143c8b85e8233ae5ba0407de
Rohit padwal <rpadwal98@gmail.com>
2021-11-17 20:08:22 -0600

    "files changed"

--- server/contactus.php
+++ server/contactus.php
@@ -9,7 +9,7 @@ $_POST = json_decode($rest_json, true);
 
 
 try{
-    $connection = new mysqli("localhost", "root", "", "test1", 4306);
+    $connection = new mysqli("localhost", "root", "", "test1");
     // Check connection
 if ($mysqli -> connect_errno) {
     echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
